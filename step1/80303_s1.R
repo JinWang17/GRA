ba70756c3aab1c4664b79e971755e2a08007175f
JinWang17 <wangjinmichelle@gmail.com>
2016-05-11 16:46:17 -0400

    Add files via upload

---
+++ step1/80303_s1.R
@@ -0,0 +1,74 @@
+rm(list=ls())
+
+library(GenABEL)
+library(survival)
+
+
+################ input #####################
+
+tfamfile="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80303_geno.tfam"
+tools::md5sum(tfamfile)
+
+tpedfile="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80303_geno.tped"
+tools::md5sum(tpedfile)
+
+phenofile="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80303_pheno.txt"
+tools::md5sum(phenofile)
+
+convert.snp.tped(tpedfile = tpedfile, tfamfile = tfamfile, outfile = "genotype.raw")
+df <- load.gwaa.data(phe = phenofile, gen = "genotype.raw", force = T)
+
+class(df)
+names(phdata(df))
+descriptives.trait(df)
+descriptives.marker(df)
+nsnps <- nsnps(gtdata(df))
+
+############ MAF ##########################################
+#mc = check.marker(df, callrate = 0.95, extr.call = 0.95, 
+#                  p.level = 1e-08, het.fdr = 0, maf = 0.01)
+#df2 = df[, !is.element(snpnames(gtdata(df)), mc$nofreq)]
+#nsnps(gtdata(df2))
+#
+#mc = check.marker(df, callrate = 0.95, extr.call = 0.95, 
+#                  p.level = 1e-08, het.fdr = 0, maf = 0.01)
+#df2 = df[, !is.element(snpnames(gtdata(df)), mc$nofreq)]
+#nsnps(gtdata(df2))
+#############################################################
+
+################ get statistics #############################
+
+snps <- as.numeric(gtdata(df))
+chr <- c(as.numeric(chromosome(gtdata(df))))
+snp <- c(colnames(snps))
+bp <- c(as.numeric(map(gtdata(df))))
+
+time <- phdata(df)$ostime
+status <- phdata(df)$osevent
+
+coef <- rep(0, nsnps)
+v <- rep(0, nsnps)
+p <- rep(0, nsnps)
+ss <- rep(0, nsnps)
+
+for(i in 1:nsnps){
+  if (i%%10000==0) print(paste(i, date()));
+  fit <- summary(coxph(Surv(time, status) ~ snps[,i]*phdata(df)$arm + snps[,i]
+                       + phdata(df)$arm + strata(phdata(df)$priorrt, phdata(df)$dzext,
+                                                 phdata(df)$ps) ))
+  coef[i] <- fit$coef[1, 1]
+  v[i] <- fit$coef[1,3]^2
+  p[i] <- fit$coef[1,5]
+  ss[i] <- fit$n
+}
+
+refallele <- refallele(gtdata(df))
+effallele <- effallele(gtdata(df))
+
+result <- data.frame(snp = snp, chr = chr, refallele = refallele, effallele = effallele,
+                     bp = bp, coef = coef, sd = sqrt(v), pvalue = p, n = ss)
+
+################ output ######################################
+
+write.table(result, "/lustre/scr/j/i/jinjin/GRA/meta_analysis/result/80303.txt", 
+            sep="\t")
---
+++ step1/80405_s1.R
@@ -0,0 +1,209 @@
+rm(list=ls())
+
+library(GenABEL)
+library(survival)
+
+################ input #####################
+
+tfamfile1="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_geno_1.tfam"
+tools::md5sum(tfamfile1)
+
+tpedfile1="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_geno_1.tped"
+tools::md5sum(tpedfile1)
+
+tfamfile2="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_geno_2.tfam"
+tools::md5sum(tfamfile2)
+
+tpedfile2="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_geno_2.tped"
+tools::md5sum(tpedfile2)
+
+phenofile="/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_pheno.txt"
+tools::md5sum(phenofile)
+
+convert.snp.tped(tpedfile = tpedfile1, tfamfile = tfamfile1, outfile = "genotype1.raw")
+convert.snp.tped(tpedfile = tpedfile2, tfamfile = tfamfile2, outfile = "genotype2.raw")
+#merge.snp.data("genotype1.raw", "genotype2.raw")
+df1 <- load.gwaa.data(phe = phenofile, gen = "genotype1.raw", force = T)
+df2 <- load.gwaa.data(phe = phenofile, gen = "genotype2.raw", force = T)
+df1 <- df1[phdata(df1)$cau == 1, ] 
+df2 <- df2[phdata(df2)$cau == 1, ] 
+mc1 = check.marker(df1, callrate = 0.95, extr.call = 0.95, 
+                  p.level = 1e-08, het.fdr = 0, ibs.exclude = "lower")
+df1 = df1[mc1$idok, mc1$snpok]
+mc2 = check.marker(df2, callrate = 0.95, extr.call = 0.95, 
+                  p.level = 1e-08, het.fdr = 0, ibs.exclude = "lower")
+df2 = df2[mc2$idok, mc2$snpok]
+nsnps(gtdata(df1))
+nsnps(gtdata(df2))
+
+df1_m = df1[, is.element(snpnames(gtdata(df1)), snpnames(gtdata(df2)))]
+df2_m = df2[, is.element(snpnames(gtdata(df2)), snpnames(gtdata(df1)))]
+df = merge(df1_m, df2_m)
+mc = check.marker(df, callrate = 0.95, extr.call = 0.95, p.level = 1e-08, 
+                  het.fdr = 0, ibs.exclude = "lower")
+df = df[mc$idok, mc$snpok]
+nsnps(gtdata(df))
+
+df1 = df1[, !is.element(snpnames(gtdata(df1)), snpnames(gtdata(df1_m)))]
+df2 = df2[, !is.element(snpnames(gtdata(df2)), snpnames(gtdata(df2_m)))]
+nsnps(gtdata(df1))
+nsnps(gtdata(df2))
+mc1 = check.marker(df1, callrate = 0.95, extr.call = 0.95, 
+                   p.level = 1e-08, het.fdr = 0, ibs.exclude = "lower")
+df1 = df1[mc1$idok, mc1$snpok]
+mc2 = check.marker(df2, callrate = 0.95, extr.call = 0.95, 
+                   p.level = 1e-08, het.fdr = 0, ibs.exclude = "lower")
+df2 = df2[mc2$idok, mc2$snpok]
+nsnps(gtdata(df1))
+nsnps(gtdata(df2))
+
+rm(df1_m)
+rm(df2_m)
+
+
+
+
+############ MAF ##########################################
+#mc = check.marker(df, callrate = 0.95, extr.call = 0.95, 
+#                  p.level = 1e-08, het.fdr = 0, maf = 0.01)
+#df2 = df[, !is.element(snpnames(gtdata(df)), mc$nofreq)]
+#nsnps(gtdata(df2))
+#
+#mc = check.marker(df, callrate = 0.95, extr.call = 0.95, 
+#                  p.level = 1e-08, het.fdr = 0, maf = 0.01)
+#df2 = df[, !is.element(snpnames(gtdata(df)), mc$nofreq)]
+#nsnps(gtdata(df2))
+#############################################################
+
+################ get statistics #############################
+
+class(df)
+names(phdata(df))
+descriptives.trait(df)
+descriptives.marker(df)
+nsnps <- nsnps(gtdata(df))
+
+
+snps <- as.numeric(gtdata(df))
+chr <- c(as.numeric(chromosome(gtdata(df))))
+snp <- c(colnames(snps))
+bp <- c(as.numeric(map(gtdata(df))))
+
+time <- phdata(df)$ostime
+status <- phdata(df)$osevent
+
+coef <- rep(0, nsnps)
+v <- rep(0, nsnps)
+p <- rep(0, nsnps)
+ss <- rep(0, nsnps)
+
+save("df" = df, file = "/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405.Rdata")
+for(i in 1:nsnps){
+  if (i%%10000==0) print(paste(i, date()));
+  fit <- summary(coxph(Surv(time, status) ~ snps[,i]*phdata(df)$Bev + snps[,i]
+                       + phdata(df)$Bev + phdata(df)$Cetux   
+                       + strata(phdata(df)$prot_chemo, phdata(df)$pr_adj, phdata(df)$pr_rad)))
+  coef[i] <- fit$coef[1, 1]
+  v[i] <- fit$coef[1,3]^2
+  p[i] <- fit$coef[1,5]
+  ss[i] <- fit$n
+}
+
+refallele <- refallele(gtdata(df))
+effallele <- effallele(gtdata(df))
+
+result <- data.frame(snp = snp, chr = chr, refallele = refallele, effallele = effallele,
+                     bp = bp, coef = coef, sd = sqrt(v), pvalue = p, n = ss)
+
+################ output ######################################
+
+write.table(result, "/lustre/scr/j/i/jinjin/GRA/meta_analysis/result/80405.txt", 
+            sep="\t")
+
+################ get statistics #############################
+
+class(df1)
+names(phdata(df1))
+descriptives.trait(df1)
+descriptives.marker(df1)
+nsnps <- nsnps(gtdata(df1))
+
+snps <- as.numeric(gtdata(df1))
+chr <- c(as.numeric(chromosome(gtdata(df1))))
+snp <- c(colnames(snps))
+bp <- c(as.numeric(map(gtdata(df1))))
+
+time <- phdata(df1)$ostime
+status <- phdata(df1)$osevent
+
+coef <- rep(0, nsnps)
+v <- rep(0, nsnps)
+p <- rep(0, nsnps)
+ss <- rep(0, nsnps)
+
+save("df1" = df1, file = "/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_1.Rdata")
+for(i in 1:nsnps){
+  if (i%%10000==0) print(paste(i, date()));
+  fit <- summary(coxph(Surv(time, status) ~ snps[,i]*phdata(df1)$Bev + snps[,i]
+                       + phdata(df1)$Bev + phdata(df1)$Cetux   
+                       + strata(phdata(df1)$prot_chemo, phdata(df1)$pr_adj, phdata(df1)$pr_rad)))
+  coef[i] <- fit$coef[1, 1]
+  v[i] <- fit$coef[1,3]^2
+  p[i] <- fit$coef[1,5]
+  ss[i] <- fit$n
+}
+
+refallele <- refallele(gtdata(df1))
+effallele <- effallele(gtdata(df1))
+
+result <- data.frame(snp = snp, chr = chr, refallele = refallele, effallele = effallele,
+                     bp = bp, coef = coef, sd = sqrt(v), pvalue = p, n = ss)
+
+################ output ######################################
+
+write.table(result, "/lustre/scr/j/i/jinjin/GRA/meta_analysis/result/80405_1.txt", 
+            sep="\t")
+
+################ get statistics #############################
+
+class(df2)
+names(phdata(df2))
+descriptives.trait(df2)
+descriptives.marker(df2)
+nsnps <- nsnps(gtdata(df2))
+
+snps <- as.numeric(gtdata(df2))
+chr <- c(as.numeric(chromosome(gtdata(df2))))
+snp <- c(colnames(snps))
+bp <- c(as.numeric(map(gtdata(df2))))
+
+time <- phdata(df2)$ostime
+status <- phdata(df2)$osevent
+
+coef <- rep(0, nsnps)
+v <- rep(0, nsnps)
+p <- rep(0, nsnps)
+ss <- rep(0, nsnps)
+
+save("df2" = df2, file = "/lustre/scr/j/i/jinjin/GRA/meta_analysis/80405_2.Rdata")
+for(i in 1:nsnps){
+  if (i%%10000==0) print(paste(i, date()));
+  fit <- summary(coxph(Surv(time, status) ~ snps[,i]*phdata(df2)$Bev + snps[,i]
+                       + phdata(df2)$Bev + phdata(df2)$Cetux   
+                       + strata(phdata(df2)$prot_chemo, phdata(df2)$pr_adj, phdata(df2)$pr_rad)))
+  coef[i] <- fit$coef[1, 1]
+  v[i] <- fit$coef[1,3]^2
+  p[i] <- fit$coef[1,5]
+  ss[i] <- fit$n
+}
+
+refallele <- refallele(gtdata(df2))
+effallele <- effallele(gtdata(df2))
+
+result <- data.frame(snp = snp, chr = chr, refallele = refallele, effallele = effallele,
+                     bp = bp, coef = coef, sd = sqrt(v), pvalue = p, n = ss)
+
+################ output ######################################
+
+write.table(result, "/lustre/scr/j/i/jinjin/GRA/meta_analysis/result/80405_2.txt", 
+            sep="\t")
