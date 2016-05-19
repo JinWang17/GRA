23e1942c951b532d4f8eb98a01eca7938f4dc7f4
JinWang17 <wangjinmichelle@gmail.com>
2016-05-19 14:49:45 -0400

    Add files via upload

--- step2/detail.R
+++ step2/detail.R
@@ -1,3 +1,5 @@
+library(qqman)
+
 wd <- "/lustre/scr/j/i/jinjin/GRA/meta_analysis/result/"
 top100 <- read.table(paste(wd, "top100.txt", sep = ""), header = TRUE)
 f80303 <- read.table(paste(wd, "80303.txt", sep = ""), header = TRUE)
@@ -14,8 +16,18 @@ sd.40503 <- rep(".", 100)
 maf.80303 <- rep(".", 100)
 maf.80405 <- rep(".", 100)
 maf.40503 <- rep(".", 100)
+maineff.80303 <- rep(0, 100)
+maineff.80405 <- rep(0, 100)
+maineff.40503 <- rep(0, 100)
+mainsd.80303 <- rep(0, 100)
+mainsd.80405 <- rep(0, 100)
+mainsd.40503 <- rep(0, 100)
+main <- rep(0, 100)
+mainsd <- rep(0, 100)
 
 for (i in 1:100){
+  tempbeta <- 0
+  tempsd <- 0
   if(is.element(snp[i], f80303$snp)){
     index <- match(snp[i], f80303$snp)
     if(top100$Allele1[i] == tolower(f80303$effallele[index])){
@@ -25,6 +37,10 @@ for (i in 1:100){
     }
     sd.80303[i] = f80303$sd[index]
     maf.80303[i] = f80303$maf[index]
+    maineff.80303[i] = f80303$maincoef[index]
+    mainsd.80303[i] = f80303$mainsd[index]
+    tempbeta <- tempbeta + maineff.80303[i]/(mainsd.80303[i]^2)
+    tempsd <- tempsd + 1.0/(mainsd.80303[i]^2)
   }
   if(is.element(snp[i], f80405$snp)){
     index <- match(snp[i], f80405$snp)
@@ -35,6 +51,10 @@ for (i in 1:100){
     }
     sd.80405[i] = f80405$sd[index]
     maf.80405[i] = f80405$maf[index]
+    maineff.80405[i] = f80405$maincoef[index]
+    mainsd.80405[i] = f80405$mainsd[index]
+    tempbeta <- tempbeta + maineff.80405[i]/(mainsd.80405[i]^2)
+    tempsd <- tempsd + 1.0/(mainsd.80405[i]^2)
   }
   if(is.element(snp[i], f40503$snp)){
     index <- match(snp[i], f40503$snp)
@@ -45,13 +65,33 @@ for (i in 1:100){
     }
     sd.40503[i] = f40503$sd[index]
     maf.40503[i] = f40503$maf[index]
+    maineff.40503[i] = f40503$maincoef[index]
+    mainsd.40503[i] = f40503$mainsd[index]
+    tempbeta <- tempbeta + maineff.40503[i]/(mainsd.40503[i]^2)
+    tempsd <- tempsd + 1.0/(mainsd.40503[i]^2)
   }
+  main[i] <- tempbeta/tempsd
+  mainsd[i] <- sqrt(1.0/tempsd)
 }
 
-detail <- data.frame(top100, eff.80303, sd.80303, maf.80303, 
+detail <- data.frame(top100, main, mainsd, eff.80303, sd.80303, maf.80303, 
                      eff.40503, sd.40503, maf.40503, eff.80405, sd.80405, maf.80405)
 
 write.csv(detail, file = paste(wd, "top100_detail.csv"))
+
+#### QQ plot ############################################################
+
+png(paste(wd, "qq80303.png", sep = ""), width=1000, height=1000, pointsize=22)
+qq(f80303$pvalue, xlim=c(0,8), ylim=c(0,8))
+dev.off()
+
+png(paste(wd, "qq80405.png", sep = ""), width=1000, height=1000, pointsize=22)
+qq(f80405$pvalue, xlim=c(0,8), ylim=c(0,8))
+dev.off()
+
+png(paste(wd, "qq40503.png", sep = ""), width=1000, height=1000, pointsize=22)
+qq(f40503$pvalue, xlim=c(0,8), ylim=c(0,8))
+dev.off()
 
 #### for alternative approach ###########################################
 
@@ -108,3 +148,13 @@ detail <- data.frame(top100, eff.80303, sd.80303, maf.80303,
                      eff.40503, sd.40503, maf.40503, eff.80405, sd.80405, maf.80405)
 
 write.csv(detail, file = paste(wd, "top100_alt_detail.csv"))
+
+#### QQ plot ############################################################
+
+png(paste(wd, "qq80303_alt.png", sep = ""), width=1000, height=1000, pointsize=22)
+qq(f80303$pvalue, xlim=c(0,8), ylim=c(0,8))
+dev.off()
+
+png(paste(wd, "qq80405_alt.png", sep = ""), width=1000, height=1000, pointsize=22)
+qq(f80405$pvalue, xlim=c(0,8), ylim=c(0,8))
+dev.off()
